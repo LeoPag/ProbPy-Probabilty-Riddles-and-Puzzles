b378c950dfd1576cb37575b8f49bf3dc6fec2ca3
Angelo Gnazzo <96598743+angelognazzo@users.noreply.github.com>
2023-06-02 17:03:32 +0200

    code and simulations upload

---
+++ Gambler's Ruin/README.md
@@ -0,0 +1,11 @@
+# The Gambler's Ruin Problem
+
+In this folder, you can find a classic probability problem involving Markov chains.
+
+The question is the following:
+
+**1) Angelo has 1$ and Leonardo has 2$. They decide to play a game against each other. At each turn of the game, the losers gives the winner 1\$. Angelo is a stronger player than Leonardo and wins each turn with probability 2/3. The game ends when one of the player goes bankrupt. What is the probability that Angelo wins the game?**
+
+We really encourage you to have a go at the problem before looking at the solution.
+You can find both the analytical solution and some simulations in Python code to verify the accuracy of our answer.
+ 
---
+++ Gambler's Ruin/gambler_ruin.py
@@ -0,0 +1,64 @@
+import random
+import numpy as np
+from scipy.stats import norm
+
+class Simulation(object):
+
+    def __init__(self, p):
+        self.p = p
+
+    def get_next_state(self,state):
+
+        movement = np.random.choice([-1, 1], p = [1-self.p, self.p]) # -1: left, 1: right
+        
+        #Start
+        if state == 0:
+            next_state = state + movement
+        # Leonardo wins: absorbing state
+        elif state == -1:
+            next_state = -1
+
+        # Transition state
+        elif state == 1:
+            next_state = state + movement
+        #Angleo wins: absorbing state 
+        elif state == 2:
+            next_state = 2
+        
+        return next_state
+
+    def run_simulation(self):
+
+        current_state = 0
+
+        while current_state != 2 or current_state != -1:
+            current_state = self.get_next_state(current_state)
+            if current_state == 2:
+                return 1 # Angelo wins
+            elif current_state == -1:
+                return 0 # Leonardo wins
+
+    """
+    Compute a confidence interval for the population proportion
+    """
+    def get_confidence_interval(self, confidence, n):
+
+        "Store observations"
+        observations = []
+        for i in range(n):
+            observations.append(self.run_simulation())
+
+        """
+        Compute sample mean and sample_variance
+        """
+        sample_proportion = sum(observations) / n
+
+        sample_variance = sample_proportion * (1 - sample_proportion)
+
+        z_alpha_halved = norm.ppf((1-confidence) / 2 + confidence, loc=0, scale=1)
+        print("The sample proportion is: ", sample_proportion)
+        print("The ", confidence*100, "% confidence interval for the mean of the population is: [",
+                sample_proportion - z_alpha_halved * (np.sqrt(sample_variance) /  np.sqrt(n)) ,
+                sample_proportion + z_alpha_halved * (np.sqrt(sample_variance) /  np.sqrt(n)),
+                "]")
+        return observations
