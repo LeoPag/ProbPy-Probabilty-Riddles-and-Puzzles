48d98af7435839134203b675c42a16ef09d46eb1
Angelo Gnazzo <96598743+angelognazzo@users.noreply.github.com>
2023-06-08 15:12:21 +0200

    Update first_ace.py

--- The First Ace/first_ace.py
+++ The First Ace/first_ace.py
@@ -1,23 +1,73 @@
-"""
-This code implemnts the algorithmic solution to Problem 40 of Mosteller book
-"""
+import itertools, random
+import numpy as np
+from scipy.stats import norm
 
-class Solution():
+class Simulation(object):
 
     def __init__(self):
-        self.probabilities = [-1] * 50 #1 based index array to store probability that ace is turned exactly at round i.
-        self.cumulatives = [0] * 50 #1 based index array to store probability that ace is turbed before or at round i.
+        self.deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+        self.aces = [(1, 'Spade'), (1, 'Heart'), (1, 'Diamond'), (1, 'Club')]
+    def run_simulation(self):
 
-    def compute_probability(self,i):
-        if(i == 1):
+        # Shuffle the deck
+        random.shuffle(self.deck)
+        
+        for i in range(len(self.deck)):
+
+            # Check if the card is an ace
+            if self.deck[i] in self.aces:
+                return i + 1, self.deck[i]
+
+    """
+    Compute a confidence interval for the mean of the population
+    """
+    def get_confidence_interval(self, confidence, n):
+
+        "Store observations"
+        observations = []
+        which_cards = []
+        for i in range(n):
+            observations.append(self.run_simulation()[0])
+            which_cards.append(self.run_simulation()[1][1])
+
+        """
+        Compute sample mean and sample_variance
+        """
+        sample_mean = sum(observations) / n
+
+        sample_variance = 0
+        for i in range(n):
+            sample_variance += (observations[i] - sample_mean) ** 2
+        sample_variance = sample_variance / (n - 1)
+
+        z_alpha_halved = norm.ppf((1-confidence) / 2 + confidence, loc=0, scale=1)
+
+        print("The average number of cards required to produce the first ace is: ", sample_mean)
+        print("The", confidence*100, "% confidence interval for the average number of cards required to produce the first ace is: [",
+                sample_mean - z_alpha_halved * (np.sqrt(sample_variance) /  np.sqrt(n)) ,
+                sample_mean + z_alpha_halved * (np.sqrt(sample_variance) /  np.sqrt(n)),
+                "]")
+        return observations, which_cards
+    
+
+class AnalyticalComputation(object):
+    # In this class we compute the expected value of the number of cards required to produce the first ace using a recursive formula
+    def __init__(self):
+        self.deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+        self.aces = [(1, 'Spade'), (1, 'Heart'), (1, 'Diamond'), (1, 'Club')]
+        self.probabilities = [-1] * 50 #1 based index array to store probability that ace is turned exactly at round i.
+        self.cumulatives = [0] * 50 #1 based index array to store probability that ace is turned before or at round i.
+
+    def compute_probability(self, turn):
+        if (turn == 1): # First turn of the cards
             self.probabilities[1] = 4/52
             self.cumulatives[1] = 4/52
             return (4/52)
         else:
-            if(self.cumulatives[i-1] == 0): raise ValueError('You must compute probability at ', i-1, ' first')
-            self.probabilities[i]  = (1 - self.cumulatives[i-1]) * (4 / (52-(i-1)))
-            self.cumulatives[i] = self.probabilities[i] + self.cumulatives[i-1]
-            return(self.probabilities[i])
+            if(self.cumulatives[turn-1] == 0): raise ValueError('You must compute probability at ', turn-1, ' first')
+            self.probabilities[turn]  = (1 - self.cumulatives[turn-1]) * (4 / (52-(turn-1)))
+            self.cumulatives[turn] = self.probabilities[turn] + self.cumulatives[turn-1]
+            return(self.probabilities[turn])
 
     def check_sum(self):
         if(self.cumulatives[49] == 0): raise ValueError('You must run the expected_value_function')
@@ -27,10 +77,4 @@ class Solution():
         E = 0
         for i in range(1,50):
             E += i * self.compute_probability(i)
-
-        return E
-
-
-sol = Solution()
-print("Average number of card to be turned is: ", sol.expected_value())
-sol.check_sum()
+        return E, self.probabilities, self.cumulatives
